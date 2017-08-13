235ce6c0051b27c252d1e515d092bcaeb5bc1dae
Mariel Salcedo <mjjsalcedo@yahoo.com>
2017-08-12 18:36:47 -1000

    created needed files and did imports

---
+++ src/containers/BookListContainer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {getBooksFromFakeXHR} from '../lib/books.db.js';
+class BookList extends React.Component {
+  constructor(props){
+    super(props);
+  }
+}
+
+export default BookList;
---
+++ src/containers/NewBookFormContainer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {addBookToFakeXHR} from '../lib/books.db.js';
+
+class NewBookForm extends React.Component {
+  constructor(props){
+    super(props);
+  }
+}
+export default NewBookForm;
