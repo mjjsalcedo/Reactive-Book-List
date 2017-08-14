f83a493544f430b9fe3b00bb0b2becd23cb1bd22
Mariel Salcedo <mjjsalcedo@yahoo.com>
2017-08-14 13:55:04 -1000

    setup file structure

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from '../logo.svg';
+import '../containers/App/App.css';
+
+var Header = () => (
+<div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+)
+
+export default Header;
