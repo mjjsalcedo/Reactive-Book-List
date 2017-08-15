ca55398087d7668b528b17937f7a78863b2ce774
Mariel Salcedo <mjjsalcedo@yahoo.com>
2017-08-14 15:19:39 -1000

    created filter componenet

---
+++ src/components/BookFilterInput.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+var SearchFilterInput = ({ setSearchFilter }) => (
+
+  <input type ="text" placeholder="search..." onChange={ setSearchFilter }/>
+)
+
+export default SearchFilterInput;
--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -3,6 +3,7 @@ import Header from '../../components/Header.js';
 import AppTitle from '../../components/BookListAppTitle.js';
 import {getBooksFromFakeXHR, addBookToFakeXHR} from '../../lib/books.db.js'
 import BookList from '../BookList/index.js'
+import SearchFilterInput from '../../components/BookFilterInput.js';
 
 class App extends Component {
 
@@ -14,8 +15,15 @@ class App extends Component {
       books: [],
       searchFilter: ''
     };
+
+  this.searchFilterInput = this.searchFilterInput.bind(this)
   }
 
+
+  searchFilterInput(event){
+    var searchFilter = event.target.value
+    this.setState = {searchFilter}
+  }
 
   componentDidMount() {
     getBooksFromFakeXHR().
